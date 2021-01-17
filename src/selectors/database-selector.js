add7d4b22a6d372b3d05226c06c0c93a89b2bcaf
hwalker928 <harry@repoleved.xyz>
2021-01-17 17:21:38 +0000

    Indents AND MORE INDENTS!!!!

--- src/selectors/database-selector.js
+++ src/selectors/database-selector.js
@@ -2,7 +2,7 @@ const drivers = require('../internals/dirscan')('../databases')
 const preferred = `${process.env.PREFERRED_DATABASE || 'lokijs'}.js`
 
 if (drivers.indexOf(preferred) === -1) {
-    global.logger.error(`No database driver available called ${preferred}, available choices: ${drivers.join(', ')}`, true)
+  global.logger.error(`No database driver available called ${preferred}, available choices: ${drivers.join(', ')}`, true)
 }
 
 if (global.logger) global.logger.debug(`Using ${preferred} database driver`)
