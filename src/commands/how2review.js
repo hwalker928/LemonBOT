c4b20d54213ecc540647915709e365b6c3add725
JustSomeRandomeGuy <38541170+JustSomeRandomeGuy@users.noreply.github.com>
2021-01-20 11:20:32 +0000

    fix

--- src/commands/how2review.js
+++ src/commands/how2review.js
@@ -1,19 +1,17 @@
-const { MessageEmbed } = require('discord.js')
-
 module.exports = {
-  meta: {
-    help: 'A guide on how to leave a review',
-    level: 1,
-    doNotDocument: true
-  },
-  exec: async (msg, suffix) => {
-    const embed = new MessageEmbed()
-      .setTitle('How to submit a review.')
-      .setDescription('To leave a review for your freelancer, you can use `!review`')
-      .addField('__Format:__', '**!review <@Freelancer> <1-10>/10 <Review Message>**')
-      .addField('__Here is an example:__', '**!review <@590651977034366976> 10/10 They did a great job!**')
-      .setFooter('Lemon Studios Review System')
-      .setColor(0xFBF514)
-    await msg.channel.send(embed)
-  }
+    meta: {
+        help: 'A guide on how to leave a review',
+        level: 1,
+        doNotDocument: true
+    },
+    exec: async (msg, suffix) => {
+        const embed = msg.channel.createEmbed()
+            .setTitle('How to submit a review.')
+            .setDescription('To leave a review for your freelancer, you can use `!review`')
+            .addField('__Format:__', '**!review <@Freelancer> <1-10>/10 <Review Message>**')
+            .addField('__Here is an example:__', '**!review <@590651977034366976> 10/10 They did a great job!**')
+            .setFooter('Lemon Studios Review System')
+            .setColor(0xFBF514)
+        await msg.channel.send(embed)
+    }
 }
--- src/internals/command-indexer.js
+++ src/internals/command-indexer.js
@@ -1,29 +1,29 @@
 const commands = require('./directory-loader')('../commands')
 
 const misc = [
-  '\u200B'
+    '\u200B'
 ]
 
 const result = {}
 const aliases = new Map()
 
 for (const cmd in commands) {
-  if (result[cmd]) global.logger.error(`Unable to register command ${cmd}, a command with this name already exists.`, true)
-  result[cmd] = commands[cmd]
-  if (commands[cmd].meta.alias) {
-    for (const x of commands[cmd].meta.alias) {
-      if (commands[x]) global.logger.error(`Cannot set alias ${x}, there's a command with this name.`, true)
-      if (aliases.has(x)) global.logger.error(`Cannot set ${x} as an alias of ${cmd}, it's already in use by ${aliases.get(x)}.`, true)
-      aliases.set(x, cmd)
+    if (result[cmd]) global.logger.error(`Unable to register command ${cmd}, a command with this name already exists.`, true)
+    result[cmd] = commands[cmd]
+    if (commands[cmd].meta.alias) {
+        for (const x of commands[cmd].meta.alias) {
+            if (commands[x]) global.logger.error(`Cannot set alias ${x}, there's a command with this name.`, true)
+            if (aliases.has(x)) global.logger.error(`Cannot set ${x} as an alias of ${cmd}, it's already in use by ${aliases.get(x)}.`, true)
+            aliases.set(x, cmd)
+        }
     }
-  }
 }
 async function helpingHand (user, context, cmd) {
-  if (aliases.has(cmd)) cmd = aliases.get(cmd)
-  if (commands[cmd]) {
-    const c = commands[cmd]
-    const name = Object.getOwnPropertyNames(commands).find(f => commands[f] === c)
-    const result = [
+    if (aliases.has(cmd)) cmd = aliases.get(cmd)
+    if (commands[cmd]) {
+        const c = commands[cmd]
+        const name = Object.getOwnPropertyNames(commands).find(f => commands[f] === c)
+        const result = [
             `Command name: \`${name}\``,
             `Explanation: \`${c.meta.help}\``,
             '',
@@ -73,39 +73,10 @@ async function helpingHand (user, context, cmd) {
     } else {
         return context.send(`No command called \`${cmd}\` registered.`)
     }
-    context.send(result.join('\n'))
-  } else if (!cmd) {
-    const categories = new Set(Object.values(commands).map(x => x.meta.module).sort())
-    const names = Object.getOwnPropertyNames(commands)
-    const res = {}
-    categories.forEach(cat => {
-      const values = Object.values(commands).filter(y => y.meta.module === cat && y.meta.level !== Infinity)
-      res[cat] = values.map(x => `${names.find(f => commands[f] === x)} = "${x.meta.help}"`).sort()
-    })
-    const result = [['[[Available Commands]]']]
-    for (const x in res) {
-      if (!(result[result.length - 1].join('\n').length > 1750)) result[result.length - 1].push(`\n[${(x === 'undefined') ? 'Uncategorised' : x}]`)
-      else result[result.length] = [`\n[${(x === 'undefined') ? 'Uncategorized' : x}]`]
-      res[x].forEach(rep => {
-        if (!(result[result.length - 1].join('\n').length > 1750)) result[result.length - 1].push(rep)
-        else result[result.length] = [rep]
-      })
-    }
-    if (context.guild) {
-      context.send('Please check your DMs!')
-      context = await global.bot.users.cache.get(user) // reassign context to be a dm channel
-    }
-    for (const x in result) {
-      context.send(`\`\`\`ini\n${result[x].join('\n')}\n\`\`\``)
-    }
-    context.send(misc.join('\n'))
-  } else {
-    return context.send(`No command called \`${cmd}\` registered.`)
-  }
 }
 
 module.exports = {
-  commands: result,
-  alias: aliases,
-  help: helpingHand
+    commands: result,
+    alias: aliases,
+    help: helpingHand
 }
